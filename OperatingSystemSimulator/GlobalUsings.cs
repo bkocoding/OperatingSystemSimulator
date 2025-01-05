8c4e49b14d4aec13297d830b5f27cbd2edd4fc21
Burak Kaan Orak <21010310020@ogrenci.bartin.edu.tr>
2025-01-05 16:19:09 +0300

    Remove unused global using directives
    Removed several global using directives from `GlobalUsings.cs` to clean up the codebase. The removed namespaces and aliases include:
    - `System.Collections.Immutable`
    - `Microsoft.Extensions.DependencyInjection`
    - `Microsoft.Extensions.Hosting`
    - `Microsoft.Extensions.Logging`
    - `Microsoft.Extensions.Options`
    - `OperatingSystemSimulator.Models`
    - `Windows.ApplicationModel.Activation.ApplicationExecutionState` (alias)
    - `CommunityToolkit.Mvvm.ComponentModel`
    - `CommunityToolkit.Mvvm.Input`

--- OperatingSystemSimulator/GlobalUsings.cs
+++ OperatingSystemSimulator/GlobalUsings.cs
@@ -1,15 +1,10 @@
-global using System.Collections.Immutable;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
-global using Microsoft.Extensions.Options;
 global using OperatingSystemSimulator.Models;
 #if MAUI_EMBEDDING
 global using OperatingSystemSimulator.MauiControls;
 #endif
-global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
-global using CommunityToolkit.Mvvm.ComponentModel;
-global using CommunityToolkit.Mvvm.Input;
 global using OperatingSystemSimulator.Pages;
 global using OperatingSystemSimulator.Pages.BIOSSettings;
 global using OperatingSystemSimulator.ViewModels.PageViewModels;
